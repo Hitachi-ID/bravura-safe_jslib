c9628135380295d5ba9f814d9cb194d204470353
Gerry Dunkelman <gerryd@hitachi-id.com>
2022-04-28 12:22:54 -0400

    Corrected missing method getInternalApplicationVersion() needed for class ElectronPlatformUtilsService that implements PlatformUtilsService.
    ^FOX-60506

--- electron/src/services/electronPlatformUtils.service.ts
+++ electron/src/services/electronPlatformUtils.service.ts
@@ -105,6 +105,10 @@ export class ElectronPlatformUtilsService implements PlatformUtilsService {
     return ipcRenderer.invoke("appVersion");
   }
 
+  getInternalApplicationVersion(): Promise<string> {
+    return Promise.resolve("22.03.01");
+  }
+
   // Temporarily restricted to only Windows until https://github.com/electron/electron/pull/28349
   // has been merged and an updated electron build is available.
   supportsWebAuthn(win: Window): boolean {
